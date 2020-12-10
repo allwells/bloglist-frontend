94427501b094f007660fade78db90a0d48dc4ce1
Allwel Onen <aleenfestus@gmail.com>
2020-12-10 22:34:08 +0100

    Modify blog lists

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -7,10 +7,10 @@ const Blogs = ({ filterBlog, deleteBlog }) => {
         <div key={i}>
           <h3>{blog.title}</h3>
           <p>
-            <i>{blog.author}</i>
+            Author: <i>{blog.author}</i>
           </p>
-          <p>{blog.url}</p>
-          <p>{blog.likes}</p>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes}</p>
           <button onClick={() => deleteBlog(blog.id, blog.title)}>
             Delete Blog
           </button>
